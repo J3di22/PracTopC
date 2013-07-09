319838b5f767d73f0f6f279f8f30470853f96073
J3di22 <J3di22@gmail.com>
2013-07-08 21:38:05 -0700

    First TC prob

---
+++ Div1/Div1/UserName.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Div1
+{
+    class UserName
+    {
+        public String newMember(String[] existingNames, String newName)
+        {
+            if (!existingNames.Contains(newName))
+            {
+                return newName;
+            }
+            else
+            {
+                // name exists, try parse first
+
+                Match match = Regex.Match(newName, @"([a-zA-Z]*)(\d*)", RegexOptions.IgnoreCase);
+
+                if (match.Success)
+                {
+                    //Console.WriteLine(match.Groups.Count);
+                    //Console.WriteLine("Key1 = " + match.Groups[1].Value);
+                    //Console.WriteLine("Key2 = " + match.Groups[2].Value);
+
+                    int start = 0;
+
+                    if (!String.IsNullOrEmpty(match.Groups[2].Value))
+                    {
+                        start = Int32.Parse(match.Groups[2].Value);
+                    }
+
+                    do
+                    {
+                        start++;
+
+                        string key = match.Groups[1].Value + start;
+                        if (!existingNames.Contains(key))
+                        {
+                            return key;
+                        }
+                    } while (start < 50);
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid newName " + newName);
+                }
+            }
+
+            return String.Empty;
+        }
+    }
+}
