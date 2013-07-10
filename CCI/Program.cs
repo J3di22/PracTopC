0fd797083dd87f8f639692bb783e9f8c9db6a694
J3di22 <J3di22@gmail.com>
2013-07-09 22:15:13 -0700

    First array solution

---
+++ CCI/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                UniqueCharacter u = new UniqueCharacter();
+                Console.WriteLine(u.Test("arsdd"));
+                Console.WriteLine(u.Test("ars"));
+                Console.WriteLine(u.Test(null));
+                Console.WriteLine(u.Test(String.Empty));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+        }
+    }
+}
---
+++ CCI/UniqueCharacter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCI
+{
+    internal class UniqueCharacter
+    {
+        internal bool Test(string str)
+        {
+            bool hasDuplicate = false;
+
+            Dictionary<char, int> map = new Dictionary<char, int>();
+
+            foreach (char c in str)
+            {
+                Console.WriteLine(c);
+                if (map.ContainsKey(c))
+                {
+                    hasDuplicate = true;
+
+                    map[c]++;
+                }
+                else
+                {
+                    map[c] = 1;
+                }
+            }
+
+            foreach (char c in map.Keys)
+            {
+                Console.WriteLine(c + "\t" + map[c]);
+            }
+
+            return hasDuplicate;
+        }
+    }
+}
